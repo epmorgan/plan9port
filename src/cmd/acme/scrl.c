14ca44c0949fd533f58ccbc26a56d8c65f2a1218
Ethan Morgan <[git@monadic.sh](mailto:git@monadic.com)>
2025-03-22 12:27:01 +0000

    feat: update colours to be mostly white, and update navigation elements to make it more minimal

--- src/cmd/acme/scrl.c
+++ src/cmd/acme/scrl.c
@@ -68,15 +68,17 @@ textscrdraw(Text *t)
 	r1.min.x = 0;
 	r1.max.x = Dx(r);
 	r2 = scrpos(r1, t->org, t->org+t->fr.nchars, t->file->b.nc);
+
 	if(!eqrect(r2, t->lastsr)){
-		t->lastsr = r2;
-		draw(b, r1, t->fr.cols[BORD], nil, ZP);
-		draw(b, r2, t->fr.cols[BACK], nil, ZP);
-		r2.min.x = r2.max.x-1;
-		draw(b, r2, t->fr.cols[BORD], nil, ZP);
-		draw(t->fr.b, r, b, nil, Pt(0, r1.min.y));
-/*flushimage(display, 1); // BUG? */
+  		t->lastsr = r2;
+  		draw(b, r1, t->fr.cols[BORD], nil, ZP);
+  		r2.max.x = r2.max.x+1;
+  		draw(b, r2, t->fr.cols[TEXT], nil, ZP);
+  		r2.min.x = r2.max.x-1;
+  		draw(b, r2, t->fr.cols[BORD], nil, ZP);
+  		draw(t->fr.b, r, b, nil, Pt(0, r1.min.y));
 	}
+
 }
 
 void
